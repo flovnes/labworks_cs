282eb302280aabc6db90924c1ea6af1034fba0ec
flovnes <whyflovnes@gmail.com>
2023-10-29 12:23:03 +0200

    ♻️ refactor: lab1 is now 1 file

--- lab1/problem_two_ejudge/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-class Program
-{
-    static void Main(string[] args)
-    {
-        float input = float.Parse(Console.ReadLine());
-        System.Console.WriteLine($"{Math.Acos(input+Math.Pow(input,2))}");
-    } 
-}
