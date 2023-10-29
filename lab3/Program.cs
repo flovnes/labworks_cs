7db0dd63c9eab2820abb71c17bdd345261103fc6
flovnes <whyflovnes@gmail.com>
2023-10-29 12:31:52 +0200

    🐛 fix: lab3 wasn't up to date

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -6,22 +6,20 @@ using System.Globalization;
 namespace LabTwo {
     class Program {
         static double TriangleArea(int Bx, int By, int Cx, int Cy) {
-            return(Math.Abs(Bx*Cy-Bx*Cy)/2.0); 
+            double area = Math.Abs(Bx*Cy-By*Cx)/2.0; 
+            // Console.WriteLine($"C({Cx};{Cy}) > площа: {area:0.0}");
+            return area;
         }
-        static void FindSmallestArea(ref int Cx, ref int Cy, ref int minArea) {
-            string[] input = System.Console.ReadLine().Trim().Split();
+        static void FindSmallestArea(int Bx, int By, ref int Cx, ref int Cy, ref double minArea) {
             int start = Math.Min(0, Math.Min(Bx,By)); // min(A(0,0) B(x,y))
             int end = Math.Max(0, Math.Max(Bx,By));
-            int Bx = input[0], By = input[1];
-
-            Console.WriteLine($"Точка B = ({Bx};{By})");
-            
             for (int x = start; x < end; x++)
             {
                 for (int y = start; y < end; y++)
                 {
-                    if (Math.Abs(Cx*By/Bx - Cy) <= 2) {
-                        double curArea = triangle_area(Bx,By,x,y);
+                    if (Math.Abs(x*By/Bx - y) <= 2) {
+                        double curArea = TriangleArea(Bx,By,x,y);
+                        
                         if (curArea < minArea && curArea > 0.0) { minArea = curArea; Cx = x; Cy = y;}
                     }
                 }
@@ -31,10 +29,13 @@ namespace LabTwo {
             Console.OutputEncoding = Encoding.UTF8;
             Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");            
             Console.WriteLine("Точка A = (0;0). Введіть точку B: (_;_)");
-            double minArea = double.MAX();
+            string[] input = System.Console.ReadLine().Trim().Split();
+            int Bx = int.Parse(input[0]), By = int.Parse(input[1]);
+            Console.WriteLine($"Точка B = ({Bx};{By})");
+            double minArea = Double.MaxValue;
             int Cx = 0, Cy = 0;
-            FindSmallestArea(ref Cx, ref Cy, ref minArea);
-            Console.WriteLine($"Найменша площа трикутника ABC дорівнює {minArea:0.01} одиниць квадратних з точками A(0;0) B({Bx};{By}) C({Cx};{Cy})");
+            FindSmallestArea(Bx, By, ref Cx, ref Cy, ref minArea);
+            Console.WriteLine($"Для B({Bx};{By}) найменша площа: '{minArea:0.0}' за C({Cx};{Cy})");
         }
     }
 }
