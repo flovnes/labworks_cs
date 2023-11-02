be648dc6970172e4fc0d8c41775b3c89bebb46ee
flovnes <whyflovnes@gmail.com>
2023-11-02 10:49:03 +0200

    🔨 chore: fixed precision

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Text;
 using System.Globalization;
-#nullable disable
 class Program {
     static void Main(string[] args) {
         Console.OutputEncoding = Encoding.UTF8;
@@ -20,11 +19,13 @@ class Program {
         return Math.Abs(Bx*Cy-By*Cx)/2.0;
     }
     static void FindSmallestArea(int Bx, int By, ref int Cx, ref int Cy, ref double minArea) {
-        for (int x = 0; Math.Abs(x) <= Math.Abs(Bx); x+=1*Math.Max(Math.Min(Bx,1),-1))
-        {
-            int y = (int)Math.Floor((double)By/(double)Bx*(double)x)+1;
+        int end = Math.Abs(Bx);
+        int step = end/Bx;
+        double ratio = (double)By/(double)Bx;
+        for (int x = 0; Math.Abs(x) <= end; x += step) {
+            int y = (int)Math.Floor(ratio * (double)x) + 1;
             double curArea = TriangleArea(Bx,By,x,y);
-            if (curArea < minArea) { minArea = curArea; Cx = x; Cy = y;}
+            if (curArea < minArea) { minArea = curArea; Cx = x; Cy = y; }
         }
     }
 }
