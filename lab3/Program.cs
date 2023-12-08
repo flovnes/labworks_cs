151ba96f2d9dfc9fe3f83f7439f16a1101b8b928
flovnes <whyflovnes@gmail.com>
2023-12-08 22:30:39 +0200

    🧨

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System.Text;
 using System.Globalization;
-class Program {
-    static void Main(string[] args) {
+
+#nullable disable
+
+class Program
+{
+    static void Main(string[] args)
+    {
         Console.OutputEncoding = Encoding.UTF8;
-        Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");  
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
 
         Console.WriteLine("Точка A = (0;0). Введіть точку B: (_;_)");
         string[] input = System.Console.ReadLine().Trim().Split();
@@ -15,17 +20,20 @@ class Program {
         Console.WriteLine($"Точка B = ({Bx};{By})");
         Console.WriteLine($"Для B({Bx};{By}) найменша площа: '{minArea:0.0}' за C({Cx};{Cy})");
     }
-    static double TriangleArea(long Bx, long By, long Cx, long Cy) {
-        return Math.Abs(Bx*Cy-By*Cx)/2.0;
+    static double TriangleArea(long Bx, long By, long Cx, long Cy)
+    {
+        return Math.Abs(Bx * Cy - By * Cx) / 2.0;
     }
-    static void FindSmallestArea(long Bx, long By, ref long Cx, ref long Cy, ref double minArea) {
+    static void FindSmallestArea(long Bx, long By, ref long Cx, ref long Cy, ref double minArea)
+    {
         long end = Math.Abs(Bx);
-        long step = end/Bx;
-        double ratio = (double)By/(double)Bx;
-        long x = 0; 
-        while (Math.Abs(x) <= end) {
+        long step = end / Bx;
+        double ratio = (double)By / (double)Bx;
+        long x = 0;
+        while (Math.Abs(x) <= end)
+        {
             long y = (long)Math.Floor(ratio * (double)x) + 1;
-            double curArea = TriangleArea(Bx,By,x,y);
+            double curArea = TriangleArea(Bx, By, x, y);
             if (curArea < minArea) { minArea = curArea; Cx = x; Cy = y; }
             x += step;
         }
